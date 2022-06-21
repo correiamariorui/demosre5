8096c05fb49a8637ffa67cbda647d92041ea2f0b
student <mario.correia@vodafone.com>
2022-06-21 13:22:10 +0000

    update v1

--- demo/src/main/java/com/mario/demo/DemoApplication.java
+++ demo/src/main/java/com/mario/demo/DemoApplication.java
@@ -2,12 +2,22 @@ package com.mario.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+@GetMapping("/hi")
+public String hi(@RequestParam(value = "myVar", defaultValue = "<p>Please use /hi?myVar=NAME") String name) {
+return String.format("My Lab 5 for mister/miss %s!", name);
 }
+
+}
+
